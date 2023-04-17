9b8e219b17f2f1bdb78e64564db567efe9bed820
paulaji <61262645+paulaji@users.noreply.github.com>
2023-04-17 15:35:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,29 @@ import os
 
 client = commands.Bot(command_prefix = "/", intents = discord.Intents.all())
 
-CHANNEL_ID = blahblahblah
+CHANNEL_ID = 1094125651960803349
 channel = client.get_channel(CHANNEL_ID)
+
+# trying to set a case where if we send something other than /feedback or /bugreport
+@client.event
+async def on_message(message):
+    if not message.author.bot:  # ignore messages sent by bots
+        channel_id = 109039309587041813543591999  # Replace with the ID of the channel where you want the bot to listen
+        if message.channel.id == channel_id:
+            # Check if the message is a command other than /feedback or /bugreport
+            if message.content.startswith("/") and message.content not in ["/feedback", "/bugreport"]:
+                response = "Grrrr! Gator here! Please use `/feedback` or `/bugreport`."
+                await message.channel.send(response)
+            else:
+                # Respond to non-command messages
+                response = "Grrrr! Gator here! Please use `/feedback` or `/bugreport`."
+                await message.channel.send(response)
+
+    await client.process_commands(message)  # process commands after checking the message
+
+
+
+
 
 # # feedback modal
 # class FeedbackModal(ui.Modal, title = "feedback/suggestion(s)"):
@@ -24,7 +45,7 @@ class FeedbackModal(ui.Modal, title="feedback/suggestion(s)"):
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        channel = client.get_channel("enter channel id here")
+        channel = client.get_channel(102025802985025894125651960803349)
         await channel.send(f"feedback/suggestion: {self.your_feedback}")
 
 
@@ -33,7 +54,7 @@ class ReportbugModal(ui.Modal, title = "bug-report regarding website/app"):
   your_bugreport = ui.TextInput(label="Enter the bug you encountered", placeholder = "bug you encountered...", style = discord.TextStyle.long)
   async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        channel = client.get_channel("enter channel id here")
+        channel = client.get_channel(1094180250825925925651960803349)
         await channel.send(f"bug-report: {self.your_bugreport}")
 
 # client = commands.Bot(command_prefix = "/", intents = discord.Intents.all())
